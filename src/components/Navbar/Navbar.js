8e154f604156f1ba5808042d4d78fd786b93f658
sherman-l <43489501+sherman-l@users.noreply.github.com>
2021-07-27 15:38:16 -0700

    Added highlighting of current page in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { PersonalNavbar, PersonalNavLink } from '../BootstrapStyled.js'
-import styles from './navbar.module.css'
+import { Link } from 'react-router-dom'
+import { PersonalNavbar } from '../BootstrapStyled.js';
 
 
 
@@ -8,13 +8,13 @@ const DarkNavbar = () => {
 	return (
 		<PersonalNavbar variant="dark" bg="dark" expand="lg" sticky="top">
 			<Container>
-				<Navbar.Brand href="/">Sherman Lok</Navbar.Brand>
+				<Navbar.Brand href="/" >Sherman Lok</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav"/>
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
-						<Nav.Link href="/experience" activeStyle="active" className={styles.navlink}>Experience</Nav.Link>
-						<Nav.Link href="/projects" activeStyle="active" className={styles.navlink}>Projects</Nav.Link>
-						<Nav.Link href="/contact" activeStyle="active" className={styles.navlink}>Contact</Nav.Link>
+						<Link className="nav-link" to="/experience">Experience</Link>
+						<Link className="nav-link" to="/projects">Projects</Link>
+						<Link className="nav-link" to="/contact">Contact</Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
