8f4a15dbd6f485d9cf479e40265774c001807a2c
sherman-l <43489501+sherman-l@users.noreply.github.com>
2021-07-28 20:06:20 -0700

    Removed Contact from Navbar

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,25 +1,26 @@
 import { Card } from 'react-bootstrap';
 import styles from './projectCard.module.css'
 const ProjectCard = (props) => {
+	const {github, title, description, technologies, type} = props;
 	return (
 		<Card bg="secondary" className={`${styles.projectCardStyle}`}>
-			<a href={`${props.github}`} className={`${styles.linkNoStyle} text-white`}>
+			<a href={`${github}`} className={`${styles.linkNoStyle} text-white`}>
 				<Card.Img 
 					variant="top" 
 					src={`${process.env.PUBLIC_URL}/project_images/${props.img}`} 
 					className={styles.projectCardImg}/>
 				<Card.Body>
 					<Card.Title>
-						{props.title + " (" + props.type + ")"}
+						{title + " (" + type + ")"}
 					</Card.Title>
 					<Card.Text>
-						{props.description}
+						{description}
 					</Card.Text>
 					<Card.Title>
 						Technologies
 					</Card.Title>
 					<Card.Text>
-						{props.technologies}
+						{technologies}
 					</Card.Text>
 				</Card.Body>
 			</a>
