acb8940e76e0f2f3dd4f841d3958788baca56028
sherman-l <43489501+sherman-l@users.noreply.github.com>
2021-08-05 13:15:17 -0700

    Destructured props for readability

--- src/components/ExperienceSection/ExperienceSection.js
+++ src/components/ExperienceSection/ExperienceSection.js
@@ -2,31 +2,41 @@ import { Row, Col } from 'react-bootstrap';
 import styles from './experienceSection.module.css';
 
 const ExperienceSection = (props) => {
+	const {
+		company, 
+		title, 
+		date, 
+		duties,
+		technologies,
+		url,
+		img,
+		alt
+ 	} = props;
 	return (
 		<>
 			<Row className='align-items-center'> 
 				<Col sm={12} md={8}>
-					<h1>{props.company}</h1>
+					<h1>{company}</h1>
 					<div className={`text-secondary`}>
-						<h3>{props.title}</h3>
-						<h6>{props.date}</h6>
+						<h3>{title}</h3>
+						<h6>{date}</h6>
 						<ul>
-							{iterateDuties(props.duties)}
+							{iterateDuties(duties)}
 						</ul>
-					{ props.technologies &&
+					{ technologies &&
 						<>
 							<h5>Technologies Used</h5>
-							<p>{props.technologies}</p> 
+							<p>{technologies}</p> 
 						</> 
 					}
 					</div>
 					
 				</Col>
 				<Col sm={12} md={4} className={`${styles.experienceLogoDiv}`}>
-					<a href={props.url}>
+					<a href={url}>
 						<img 
-							src={`${process.env.PUBLIC_URL}/experience_images/${props.img}`} 
-							alt={props.alt} 
+							src={`${process.env.PUBLIC_URL}/experience_images/${img}`} 
+							alt={alt} 
 							className={`img-thumbnail rounded ${styles.experienceLogo}`}
 						/>
 					</a>
--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,13 +1,20 @@
 import { Card } from 'react-bootstrap';
 import styles from './projectCard.module.css'
 const ProjectCard = (props) => {
-	const {github, title, description, technologies, type} = props;
+	const { 
+		github, 
+		img,
+		title, 
+		description, 
+		technologies, 
+		type 
+	} = props;
 	return (
 		<Card bg="secondary" className={`${styles.projectCardStyle}`}>
 			<a href={`${github}`} className={`${styles.linkNoStyle} text-white`}>
 				<Card.Img 
 					variant="top" 
-					src={`${process.env.PUBLIC_URL}/project_images/${props.img}`} 
+					src={`${process.env.PUBLIC_URL}/project_images/${img}`} 
 					className={styles.projectCardImg}/>
 				<Card.Body>
 					<Card.Title>
